4882f864c0b5a7a6f475fe3847b6db5bb139c576
athenashopvn888 <athenashopvn888@gmail.com>
2026-07-13 04:36:51 +0700

    Add unique resource guides

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -67,6 +67,7 @@ export default function Footer() {
               <Link href="/info/weed-store-near-mississauga">Weed Store Near Mississauga</Link>
               <Link href="/weed-dispensary-north-york/">Planet x Cannabis Weed Dispensary in North York</Link>
               <Link href="/contact">Contact Us</Link>
+                          <Link href="/resources">Resources</Link>
             </nav>
           </div>
         </div>
