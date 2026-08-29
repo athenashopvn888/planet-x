c4f0a9638a10d146f1973cf571cf7bb29292bca9
athenashopvn888 <athenashopvn888@gmail.com>
2026-08-30 04:53:11 +0700

    Add Preferred Sources to PLX01 winner (#18)

--- app/components/GBPLandingPage.tsx
+++ app/components/GBPLandingPage.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import styles from "./GBPLandingPage.module.css";
 import { gbpLocation } from "../lib/gbp-location";
+import PreferredSourceButton from "./PreferredSourceButton";
 
 // Dictionary mapping category names to their respective paths
 const categoryLinks: { [key: string]: string } = {
@@ -274,6 +275,7 @@ export function GBPLandingPage() {
           )}
         </div>
       </section>
+      <PreferredSourceButton />
     </div>
   );
 }
