ccfe236eb34c6aa76133ddde464746a79b06760f
athenashopvn888 <athenashopvn888@gmail.com>
2026-09-14 12:33:15 +0700

    Add fleet cigarette deal homepage banners

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
+import FleetAnnouncementBanner from "./components/FleetAnnouncementBanner";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FlowerCard from "./components/FlowerCard";
@@ -185,6 +186,7 @@ export default function HomePage() {
 
   return (
     <main className={styles.main}>
+      <FleetAnnouncementBanner />
       {/* ── NAVBAR ── */}
       <Navbar />
 
